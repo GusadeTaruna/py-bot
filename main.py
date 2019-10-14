bfef561a4f3e30697ae9858f256b937e1e59aff1
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-14 23:53:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,21 +18,45 @@ app = Flask(__name__)
     
 
 # create a route for webhook
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET','POST'])
 def webhook():
-    req = request.get_json(force=True)
+    req = request.get_json(silent=True, force=True)
     try:
         action = reg['queryResult']['action']
     except AttributeError:
         return 'json error'
+
     if action == 'check_balance':
-        reply = {'fulfillmentText': 'kec'}
-    elif action == 'booking':
-        reply = {'fulfillmentText': 'buks'}
+        res = check_balance(req)
     else:
-        reply = {'fulfillmentText': 'hm'}
-    # return response
-    return jsonify(reply)
+        log.error('Unexpected action.')
+
+    print('Action: ' + action)
+    print('Response: ' + res)
+
+    return make_response(jsonify({'fulfillmentText': res}))
+
+def check_balance(req):
+    """Returns a string containing text with a response to the user
+    with the weather forecast or a prompt for more information
+    Takes the city for the forecast and (optional) dates
+    uses the template responses found in weather_responses.py as templates
+    """
+    parameters = req['queryResult']['parameters']
+
+    print('Dialogflow Parameters:')
+    print(json.dumps(parameters, indent=4))
+
+    # validate request parameters, return an error if there are issues
+    error, forecast_params = validate_params(parameters)
+    if error:
+        return error
+
+    if parameters.get('account')
+        response = 'respon1'
+
+    return response
+
 
 # run the app
 if __name__ == '__main__':
