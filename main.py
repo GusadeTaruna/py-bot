7bcd9ded4ab7d1dace2fdc32ba92651bb4743caf
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-15 23:50:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,15 +35,14 @@ def results():
         parameters = req['queryResult']['parameters']
         inputan = req['queryResult']['queryText']
         if parameters.get('kode'):
-            bal = ''
             # if str(parameters.get('ucapan')) == str('Hai'.lower()):
             sql = "select nama_karyawan from tb_karyawan where kode_karyawan=%s"
             cursor.execute(sql, (inputan,))
             records = cursor.fetchall()
             for row in records:
                 bal = row[0]
-                # balasan = 'Selamat Datang %s' % bal
-            return {'fulfillmentText': 'Selamat Datang %s' % bal}
+                balasan = 'Selamat Datang %s' % bal
+                return {'fulfillmentText': balasan}
             else:
                 balasan = 'ID Karyawan tidak dikenali\nCoba input lagi'
                 return {'fulfillmentText': balasan}
