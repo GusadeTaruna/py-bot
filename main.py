14c7d50a4f10dd40263c92ad1be803d8f6ec6658
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-15 22:09:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,14 +35,15 @@ def results():
         parameters = req['queryResult']['parameters']
         inputan = req['queryResult']['queryText']
         if parameters.get('kode'):
-            # if str(parameters.get('ucapan')) == str('Hai'.lower()):
-            sql = "select nama_karyawan from tb_karyawan where kode_karyawan=%s"
+            if str(parameters.get('ucapan')) == str('Hai'.lower()):
+            sql = "select nama_karyawan from tb_karyawan"
             cursor.execute(sql, (inputan,))
             records = cursor.fetchall()
             for row in records:
-                bal = row[0]
-                reply = {'fulfillmentText': bal,}
-                return jsonify(reply)
+                bal = row[]
+                bal.append(row[0])
+                balasan = 'Selamat Datang %s' % bal
+            return {'fulfillmentText': balasan}
             else:
                 balasan = 'ID Karyawan tidak dikenali\nCoba input lagi'
                 return {'fulfillmentText': balasan}
