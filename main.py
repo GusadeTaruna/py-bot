5f5ef7e32ad8381a712c24282e3575a113493444
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-15 21:27:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def results():
             for row in records:
                 alamat = row[0]
                 nama = row[1]
-                balasan = 'Selamat Datang %s, kamu tinggal di %s' %nama %alamat
+                balasan = 'Selamat Datang %s' %nama+' , Tempat tinggal %s'%alamat
                 return {'fulfillmentText': balasan}
             else:
                 balasan = 'ID Karyawan tidak dikenali\nCoba input lagi'
